35bc91392308b935a1e5ff0ffeb5d9350f3e4827
dongkelun <dongkelun@xlpayment.com>
2020-03-14 15:31:30 +0800

    增加Excel行数大于100万的情况，如果大于100万则生成两个sheet页

--- data/csv/csv2Excel.py
+++ data/csv/csv2Excel.py
@@ -8,12 +8,18 @@ def csv2excel(file_path):
     start_time = time.time()
     file = pd.read_csv(file_path, low_memory=False, dtype="str")
     print(file.head(10))
-    file.to_excel(file_path.replace('.csv', '.xlsx'), index=False)
+
+    if len(file) < 1000000:
+        file.to_excel(file_path.replace('.csv', '.xlsx'), index=False)
+    else:
+        with pd.ExcelWriter(file_path.replace('.csv', '.xlsx')) as writer:
+            file[:600000].to_excel(writer, sheet_name="1")
+            file[600000:].to_excel(writer, sheet_name="2")
+
     end_time = time.time()
+
     print('total coast ' + str(end_time - start_time) + ' s')
 
 
 if __name__ == '__main__':
     csv2excel("C:Users\\14123\Desktop\icbc\icbc_01.csv")
-
-
